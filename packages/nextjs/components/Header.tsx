49c54a55e9e61bbd968a5c3b0997bd4b6499f366
MrLaki5 <milan@darwindigital.com>
2023-06-05 21:01:12 +0200

    add: game screen ui

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -26,7 +26,7 @@ export const Header = () => {
   );
 
   return (
-    <div className="sticky lg:static top-0 navbar min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary">
+    <div className="sticky lg:static top-0 navbar min-h-0 flex-shrink-0 justify-between z-20">
       <div className="navbar-start w-auto lg:w-1/2">
         <div className="lg:hidden dropdown" ref={burgerMenuRef}>
           <button
