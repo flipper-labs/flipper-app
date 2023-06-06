2cf40d898b1929a0303291484f2a482494910421
igorroncevic <ironcevic98@gmail.com>
2023-06-06 17:28:15 +0200

    Resolve merge

--- packages/nextjs/components/stats/PreviousMatchPreview.tsx
+++ packages/nextjs/components/stats/PreviousMatchPreview.tsx
@@ -15,9 +15,15 @@ export const PreviousMatchPreview = ({ match, player }: MatchPreviewProps) => {
       border-[1px] border-solid rounded-lg border-gray-400
       `}
     >
-      <MatchPreviewUser address={match.player1.wallet} stake={match.player1.nfts ? match.player1.nfts.length : 0} />
+      <MatchPreviewUser
+        address={match.player1.wallet === player ? "You" : match.player1.wallet}
+        stake={match.player1.nfts ? match.player1.nfts.length : 0}
+      />
       <div className="text-lg">VS</div>
-      <MatchPreviewUser address={match.player2.wallet} stake={match.player2.nfts ? match.player2.nfts.length : 0} />
+      <MatchPreviewUser
+        address={match.player2.wallet === player ? "You" : match.player2.wallet}
+        stake={match.player2.nfts ? match.player2.nfts.length : 0}
+      />
       <WinLoss winner={match.winner} player={player} />
     </div>
   );
